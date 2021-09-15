3c9c61508dfbfb5cdf0a0b8b8f30ad59c39684d4
octopus <guimmm@126.com>
2021-09-15 19:56:27 +0800

    修改pom

--- provider-9001/src/main/java/com/zgm/cloud/Provider9001.java
+++ provider-9001/src/main/java/com/zgm/cloud/Provider9001.java
@@ -1,6 +1,5 @@
 package com.zgm.cloud;
 
-import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,7 +12,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoDataSourceProxy
 public class Provider9001 {
 
     public static void main(String[] args) {
